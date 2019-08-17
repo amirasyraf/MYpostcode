ac04c8e2ac7ac5bb009baed8c53b1193f028be3e
amirasyraf <amirasyraf@outlook.com>
2019-08-17 04:41:10 +0000

    Search with autosuggest

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 let textInput = document.getElementById('input');
-
 let timeout = null;
 
 textInput.onkeydown = function (e) {
     clearTimeout(timeout);
 
     timeout = setTimeout(function () {
+        let query = {query: textInput.value};
+        console.log(query);
         const ul = document.getElementById('location');
-        const url = encodeURI('http://35.247.157.195:9200/postcode/_search?size=1&q=area:' + textInput.value);
-        console.log(url);
-        fetch(url)
+        // const url = encodeURI('http://35.247.157.195:9200/postcode/_search?size=1&q=area:' + textInput.value);
+        const url = 'server.php';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+              "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            body: 'query=' +textInput.value
+        })
         .then((resp) => resp.json())
         .then(function(data) {
-        let location = data.hits.hits;
-        return location.map(function(location) {
-          let li = createNode('li'),
-              span = createNode('span');
-          span.innerHTML = `${location._source.area} ${location._source.postcode}`;
-          append(li, span);
-          append(ul, li);
-        })
+            console.log(data);
+            let location = data.hits.hits;
+            return location.map(function(location) {
+              let li = createNode('li'),
+                  span = createNode('span');
+              span.innerHTML = `${location._source.area} ${location._source.postcode}`;
+              append(li, span);
+              append(ul, li);
+            })
         })
         .catch(function(error) {
             console.log(error);
         });
-    }, 300);
+    }, 250);
 }
 
 
